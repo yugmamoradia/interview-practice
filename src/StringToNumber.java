515e764201e2850da05c95bbbc1bea1da81817b3
Yugma Moradia <yugma93@gmail.com>
2018-06-10 16:16:04 -0700

    Adding more practice programs

---
+++ src/StringToNumber.java
@@ -0,0 +1,29 @@
+
+public class StringToNumber {
+    public static void main(String[] args) {
+        System.out.println(convertString("0"));
+
+    }
+    public static Integer convertString(String input){
+
+        if(input!= null || !input.equals("")){
+            char[] inputString = input.toCharArray();
+            int result = 0;
+
+            boolean isNegative = false;
+            if(inputString[0] == '-')
+                isNegative = true;
+
+            for(int i = 0; i<inputString.length; i++){
+                if(inputString[i] == '-')
+                    continue;
+                int value = inputString[i] - '0'; //subtracting thSystem.out.println("Current value: " +value);
+                result = result*10 + value;                 //calculating the result by appending each character to integer
+            }
+            return isNegative ? result*-1 : result;
+        }
+        else    // covers null and ""
+            throw new IllegalArgumentException("Not a valid input");
+
+    }
+}
