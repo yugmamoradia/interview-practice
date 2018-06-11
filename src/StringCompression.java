4dffd99a15f21474ec844ebd822a9458903aa768
Yugma Moradia <yugma93@gmail.com>
2018-06-10 19:27:10 -0700

    Adding string compression algorithm

---
+++ src/StringCompression.java
@@ -0,0 +1,33 @@
+public class StringCompression {
+    public static void main(String[] args) {
+        System.out.println(compressString("aaaabbbbcccdddeee"));
+    }
+    public static String compressString(String input){
+        StringBuffer result = new StringBuffer();
+        char last = input.charAt(0);
+        if(input == null)
+            throw new NullPointerException("Input is null");
+        if(input == "")
+            return "Input is empty";
+        int count = 0;
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == last){
+                count++;
+            }
+            else{
+                result.append(last + "" + count);
+                count = 1;
+            }
+            last = input.charAt(i);
+        }
+        result.append(last + "" +count);
+
+        String resultString = result.toString();
+
+        if(resultString.length() <= input.length())
+            return resultString;
+        else
+            return input;
+    }
+}
+
