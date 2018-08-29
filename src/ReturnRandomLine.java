d0b21cee9cedb6843a4ca91d83a44c88e8aa36a9
Yugma Moradia <yugma93@gmail.com>
2018-08-29 16:55:32 -0700

    creating a new branch for the program to return a random line number

---
+++ src/ReturnRandomLine.java
@@ -0,0 +1,9 @@
+/**
+ * We have a huge file as an input and we want to return a random line from the file everytime we call this
+ * function. Every line should have equal probability.
+ */
+public class ReturnRandomLine {
+    public static void main(String[] args) {
+
+    }
+}
